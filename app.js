d13ca420dbe66609d4b16a0278624a33074de8fc
hadimktri <116309036+hadimktri@users.noreply.github.com>
2023-02-15 12:58:10 -0800

    lay

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
+const fs = require('fs');
 
-const fs=require('fs');
+const readCSV = file => new Promise((res, rej) => !file ?
+    rej(new Error('CSV-File not found')) :
+    fs.readFile(file, 'utf8', (err, data) => err ?
+        rej(new Error('Issue in reading the CSV-File')) :
+        res(data)));
 
-const readCSV = file => new Promise((res, rej) => !file ? rej(new Error('CSV-File not found')) :
-    fs.readFile(file, 'utf8', (err, data) => err ? rej(new Error('Issue in reading the CSV-File')) : res(data)));
+const menu = {};
+let txt = '';
+const makeMenu = (data => new Promise((res, rej) => {
+    !data || typeof data !== 'string' ?
+        rej(new Error('Menu can not be created.')) :
+        Object.values(data.split(/\r\n/)).map(txt => txt.split(',')).forEach(meal => !menu[meal[0]] ?
+            menu[meal[0]] = [[meal[3].slice(1), meal[1], meal[2]]] :
+            menu[meal[0]].push([meal[3].slice(1), meal[1], meal[2]]));
 
-const writeTxt = data => new Promise((res, rej) => !data || typeof data !== 'string' ? rej(new Error('Issue in reading menu')) :
-    res(fs.writeFile('menu.txt', data, err => err ? console.log(err) : '')));
+    Object.values(menu).forEach(meal => meal.sort((a, b) => a[1] > b[1] ? 1 : -1));
 
-const makeMenu = (text => new Promise((res, rej) => {
-    !text || typeof text !== 'string' ? rej(new Error('Menu can not be created.')) : console.log(`\n${text}\n`);
-
-    const menu = [];
-    Object.values(text.split(/\r\n/)).map(txt => txt.split(',')).forEach(meal => !menu[meal[0]] ?
-        menu[meal[0]] = [[meal[3].slice(1), meal[1], meal[2]]] : menu[meal[0]].push([meal[3].slice(1), meal[1], meal[2]]));
-        
-    Object.values(menu).forEach(meal => meal.sort((x, y) => x[1] > y[1] ? 1 : -1));
-
-    let txt = '';
     Object.keys(menu).forEach(meal => {
         txt += `\n* ${meal.charAt(0).toUpperCase() + meal.slice(1)} Items *\n`;
-        menu[meal].forEach(el => txt += `$${(el[0] * 1.8).toFixed(2)} ${el[1]}, ${el[2]}\n`);
-    })
-    console.log(txt + '\n');
+        menu[meal].forEach(el => txt += `$${(el[0] * 1.8).toFixed(2)} ${el[1]}, ${el[2]}\n`)
+    });
+    
     res(txt);
+    console.log(`\n${data}`);
+    console.log(txt + '\n');
 }));
+
+const writeTxt = txt => new Promise((res, rej) => !txt || typeof txt !== 'string' ?
+    rej(new Error('Issue in reading menu')) :
+    res(fs.writeFile('menu.txt', txt, err => err ? console.log(err) : '')));
+
 readCSV('./menu.csv').then(file => (makeMenu(file))).then(txt => writeTxt(txt)).catch(err => console.log(`\n${err}\n`));
